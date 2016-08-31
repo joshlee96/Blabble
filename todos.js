d7b98cde18ff5c9ba682573880b13ca8fa67fecd
Josh Lee <Josh Lee>
2016-08-31 15:45:42 -0700

    Added mongo

--- todos.js
+++ todos.js
@@ -1,28 +1,53 @@
 var express = require('express');
-
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
 
-var toDoItems = [
-    {id: 1, desc: "Buy Milk"},
-    {id: 2, desc: "Drop Ben off"},
-    {id: 3, desc: "Download Music"}
-];
+var url = 'mongodb://localhost:27017/test';
+
+var resultArray = [];
 
 router.get('/' , function(req, res) {
     res.render('index', {
-        title: "ToDo List App",
-        items: toDoItems
+        items: resultArray
     });
 });
 
+router.get('/get-data', function(req, res) {
+   mongo.connect(url, function(err, db) {
+       assert.equals(null, err);
+       var cursor = db.collection('blog-posts').find(); //get all pointers to these entries
+       cursor.forEach(function(doc, err) {
+           assert.equals(null, err);
+           resultArray.push(doc);
+       }, function() {
+           db.close();
+           res.render('index', {
+               items: resultArray
+           });
+       });
+   });
+    res.redirect('/')
+});
+
 router.post('/add', function(req, res){
-    var newItem = req.body.newItem;
-    console.log(newItem);
-    toDoItems.push({
-        id: toDoItems.length + 1,
-        desc: newItem
+    var newPost = {
+        name: req.body.name,
+        comment: req.body.comment
+    }
+
+    mongo.connect(url, function (err, db) {
+        assert.equal(null, err);
+        db.collection('blog-posts').insertOne(newPost, function(err, result){
+            assert.equal(null, err);
+            console.log('Item added to database');
+            db.close();
+        });
     });
     res.redirect('/');
 });
 
+router.post('/delete', function(req, res) {
+
+});
 module.exports = router;
